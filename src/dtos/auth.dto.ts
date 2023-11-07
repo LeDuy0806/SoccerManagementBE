d84ccf06920c2ea5bc93d850c4141b3afd8e7f57
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-11-07 09:30:30 +0700

    Config

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class LoginDto {
+    @IsEmail()
+    public email: string;
+
+    @IsString()
+    public password: string;
+}
+
+export class RefreshTokenDto {
+    @IsString()
+    public refreshToken: string;
+}
